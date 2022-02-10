f9b8e7f3f7dcba368477743aed144d65817727d3
Math49 <45720517+Math49@users.noreply.github.com>
2022-02-10 12:06:07 +0100

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,8 @@
+var $dialog = document.getElementById('mydialog'); 
+if(!('show' in $dialog)){ 
+    document.getElementById('promptCompat').className = 'no_dialog'; 
+}
+
+$dialog.addEventListener('close', function() { 
+    console.log('Fermeture. ', this.returnValue); 
+}); 
